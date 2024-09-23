3263de811c26b248352a9e525fe10f0fbd9e7352
ST10048211 <st10048211@vcconnect.edu.za>
2024-09-23 11:27:03 +0200

    Fixed the secrets

--- firebase.js
+++ firebase.js
@@ -1,6 +1,9 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+//const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); // Load from environment variable
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
